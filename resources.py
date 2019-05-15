caa86c27c58e60956c918e1bd42ee5347a7e7614
olivierdalang <olivier.dalang@gmail.com>
2019-05-15 16:02:53 +0200

    (cosmetic refactoring)

--- resources.py
+++ resources.py
@@ -2,56 +2,22 @@ import os
 
 from qgis.PyQt.QtGui import QIcon
 
-logo = QIcon(os.path.join(os.path.dirname(__file__), "resources", "ttp_logo.svg"))
 
-icon_tiles = QIcon(
-    os.path.join(
-        os.path.dirname(__file__), "resources", "icons/icon_background_tiles_16x16.svg"
-    )
-)
-icon_config = QIcon(
-    os.path.join(
-        os.path.dirname(__file__), "resources", "icons/icon_configuration_16x16.svg"
-    )
-)
-icon_general = QIcon(
-    os.path.join(os.path.dirname(__file__), "resources", "icons/icon_general_16x16.svg")
-)
-icon_geocoding = QIcon(
-    os.path.join(
-        os.path.dirname(__file__), "resources", "icons/icon_geocoding_32x32.svg"
-    )
-)
-icon_help = QIcon(
-    os.path.join(os.path.dirname(__file__), "resources", "icons/icon_help_16x16.svg")
-)
-icon_routes_advanced = QIcon(
-    os.path.join(os.path.dirname(__file__), "resources", "icons/icon_routes_16x16.svg")
-)
-icon_routes_simple = QIcon(
-    os.path.join(os.path.dirname(__file__), "resources", "icons/icon_routes_16x16.svg")
-)
-icon_time_filter_advanced = QIcon(
-    os.path.join(
-        os.path.dirname(__file__),
-        "resources",
-        "icons/icon_time_filter_advanced_16x16.svg",
-    )
-)
-icon_time_filter_simple = QIcon(
-    os.path.join(
-        os.path.dirname(__file__),
-        "resources",
-        "icons/icon_time_filter_simple_16x16.svg",
-    )
-)
-icon_time_map_advanced = QIcon(
-    os.path.join(
-        os.path.dirname(__file__), "resources", "icons/icon_time_map_advanced_16x16.svg"
-    )
-)
-icon_time_map_simple = QIcon(
-    os.path.join(
-        os.path.dirname(__file__), "resources", "icons/icon_time_map_simple_16x16.svg"
-    )
-)
+def icon(icon_name):
+    return QIcon(os.path.join(os.path.dirname(__file__), "resources", icon_name))
+
+
+logo = icon("ttp_logo.svg")
+
+icon_tiles = icon("icons/icon_background_tiles_16x16.svg")
+icon_config = icon("icons/icon_configuration_16x16.svg")
+icon_general = icon("icons/icon_general_16x16.svg")
+icon_geocoding = icon("icons/icon_geocoding_32x32.svg")
+icon_reverse_geocoding = icon("icons/icon_geocoding_32x32.svg")
+icon_help = icon("icons/icon_help_16x16.svg")
+icon_routes_advanced = icon("icons/icon_routes_16x16.svg")
+icon_routes_simple = icon("icons/icon_routes_16x16.svg")
+icon_time_filter_advanced = icon("icons/icon_time_filter_advanced_16x16.svg")
+icon_time_filter_simple = icon("icons/icon_time_filter_simple_16x16.svg")
+icon_time_map_advanced = icon("icons/icon_time_map_advanced_16x16.svg")
+icon_time_map_simple = icon("icons/icon_time_map_simple_16x16.svg")
