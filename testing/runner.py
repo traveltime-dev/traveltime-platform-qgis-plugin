c64c226642201dc16d530b88d1a1bfa1941842e5
Olivier Dalang <olivier.dalang@gmail.com>
2022-12-25 17:33:43 +0100

    improve runner (show output in realtime)

--- testing/runner.py
+++ testing/runner.py
@@ -4,16 +4,24 @@ accordingly to test results, working around the issue about QGIS crashing on exi
 """
 
 import sys
-from subprocess import PIPE, STDOUT, run
+from subprocess import PIPE, STDOUT, Popen
 
 print("Starting qgis...")
 
-process = run(["qgis"], stdout=PIPE, stderr=STDOUT, encoding="utf-8")
+# Run QGIS in a subprocess
+process = Popen("qgis", stdout=PIPE, stderr=STDOUT, encoding="utf-8")
 
-print(process.stdout)
+# Print output in realtime, keep full output in string
+full_output = ""
+while True:
+    output = process.stdout.readline()
+    print(output.strip(), flush=True)
+    full_output += output
+    if output == "" and process.poll() is not None:
+        break
 
-
-if "__SUCCESS__" in process.stdout and "__FAILURE__" not in process.stdout:
+# Set exit code
+if "__SUCCESS__" in full_output and "__FAILURE__" not in full_output:
     sys.exit(0)
 else:
     sys.exit(1)
