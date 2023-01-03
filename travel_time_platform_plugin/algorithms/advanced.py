fb4562ddc552b4a940ac639b81511a255d53f2a7
Olivier Dalang <olivier.dalang@gmail.com>
2023-01-03 15:33:20 +0100

    include location_id column in time filter output

--- travel_time_platform_plugin/algorithms/advanced.py
+++ travel_time_platform_plugin/algorithms/advanced.py
@@ -901,6 +901,7 @@ class TimeFilterAlgorithm(_SearchAlgorithmBase):
 
         output_fields = QgsFields(locations.fields())
         output_fields.append(QgsField("search_id", QVariant.String, "text"))
+        output_fields.append(QgsField("location_id", QVariant.String, "text"))
         output_fields.append(QgsField("reachable", QVariant.Int, "int"))
 
         for prop in self.enabled_properties():
@@ -928,6 +929,7 @@ class TimeFilterAlgorithm(_SearchAlgorithmBase):
                 for properties in location["properties"]:
                     feature = clone_feature(location["id"])
                     feature.setAttribute("search_id", result["search_id"])
+                    feature.setAttribute("location_id", location["id"])
                     feature.setAttribute("reachable", 1)
                     for prop in self.enabled_properties():
                         feature.setAttribute(
@@ -937,6 +939,7 @@ class TimeFilterAlgorithm(_SearchAlgorithmBase):
             for id_ in result["unreachable"]:
                 feature = clone_feature(id_)
                 feature.setAttribute("search_id", result["search_id"])
+                feature.setAttribute("location_id", id_)
                 feature.setAttribute("reachable", 0)
                 sink.addFeature(feature, QgsFeatureSink.FastInsert)
 
