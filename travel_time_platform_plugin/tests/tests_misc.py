75cf5380ad4b0c02d4d64ff87056393ba8ef6a67
Olivier Dalang <olivier.dalang@gmail.com>
2023-01-03 22:59:11 +0100

    fix tiles actions (failing test)

---
+++ travel_time_platform_plugin/tests/tests_misc.py
@@ -0,0 +1,42 @@
+from qgis.gui import QgsBrowserTreeView
+from qgis.PyQt.QtWidgets import QDockWidget
+from qgis.utils import iface
+
+from .base import TestCaseBase
+
+
+class MiscTest(TestCaseBase):
+    """Testing other features"""
+
+    def test_loading_map_tiles(self):
+
+        browser = iface.mainWindow().findChild(QDockWidget, "Browser")
+        treeview = browser.findChild(QgsBrowserTreeView)
+        model = treeview.model()
+
+        # Hide the browser
+        browser.setVisible(False)
+        # Select the first item
+        treeview.setCurrentIndex(model.index(0, 0))
+
+        self._feedback()
+
+        # Ensure the browser is hidden
+        self.assertFalse(browser.isVisible())
+        # Ensure the XYZ layer is not selected
+        self.assertNotEqual(
+            model.data(treeview.selectedIndexes()[0]),
+            "TravelTime - Lux",
+        )
+
+        # Use the action
+        self.plugin.action_show_tiles.trigger()
+        self._feedback()
+
+        # Ensure the panel got shown
+        self.assertTrue(browser.isVisible())
+        # Ensure the XYZ layer got selected
+        self.assertEqual(
+            treeview.model().data(treeview.selectedIndexes()[0]),
+            "TravelTime - Lux",
+        )
